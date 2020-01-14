da488ec405cbd46307b3a1791983967af9fcd1de
Patrick Dang <patrickdang6356@gmail.com>
2020-01-13 19:26:40 -0600

    clean up

--- ppmapp-javaspringboot-backend/src/test/java/com/springboot/ppmapp/PpmtoolApplicationTests.java
+++ ppmapp-javaspringboot-backend/src/test/java/com/springboot/ppmapp/PpmtoolApplicationTests.java
@@ -1,12 +1,13 @@
-package io.agileintelligence.ppmtool;
+package com.springboot.ppmapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.springboot.ppmapp.PpmtoolApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = PpmtoolApplication.class)
 public class PpmtoolApplicationTests {
 
     @Test
